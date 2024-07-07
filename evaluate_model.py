be2d1a518d5dfd4649c8196d450f977c90a6d8fb
Ronvidev <ronaldjosue2008@gmail.com>
2024-07-06 21:53:45 -0500

    fix evaluate_model

--- evaluate_model.py
+++ evaluate_model.py
@@ -17,32 +17,33 @@ def evaluate_model(model, threshold=0.7):
         video = cv2.VideoCapture(0)
         
         while video.isOpened():
-            _, frame = video.read()
+            ret, frame = video.read()
+            if not ret: break
 
-            image, results = mediapipe_detection(frame, holistic_model)
+            results = mediapipe_detection(frame, holistic_model)
             kp_sequence.append(extract_keypoints(results))
             
             if len(kp_sequence) > MAX_LENGTH_FRAMES and there_hand(results):
                 count_frame += 1
                 
-            else:
-                if count_frame >= MIN_LENGTH_FRAMES:
-                    res = model.predict(np.expand_dims(kp_sequence[-MAX_LENGTH_FRAMES:], axis=0))[0]
+            elif count_frame >= MIN_LENGTH_FRAMES:
+                last_frames = kp_sequence[-MAX_LENGTH_FRAMES:]
+                res = model.predict(np.expand_dims(last_frames, axis=0))[0]
 
-                    if res[np.argmax(res)] > threshold:
-                        sent = actions[np.argmax(res)]
-                        sentence.insert(0, sent)
-                        text_to_speech(sent)
-                        sentence, repe_sent = format_sentences(sent, sentence, repe_sent)
-                        
-                    count_frame = 0
-                    kp_sequence = []
+                if res[np.argmax(res)] > threshold:
+                    sent = actions[np.argmax(res)]
+                    sentence.insert(0, sent)
+                    text_to_speech(sent)
+                    sentence, repe_sent = format_sentences(sent, sentence, repe_sent)
+                    
+                count_frame = 0
+                kp_sequence = []
             
-            cv2.rectangle(image, (0,0), (640, 35), (245, 117, 16), -1)
-            cv2.putText(image, ' | '.join(sentence), FONT_POS, FONT, FONT_SIZE, (255, 255, 255))
+            cv2.rectangle(frame, (0,0), (640, 35), (245, 117, 16), -1)
+            cv2.putText(frame, ' | '.join(sentence), FONT_POS, FONT, FONT_SIZE, (255, 255, 255))
             
-            draw_keypoints(image, results)
-            cv2.imshow('Traductor LSP', image)
+            draw_keypoints(frame, results)
+            cv2.imshow('Traductor LSP', frame)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
                     
